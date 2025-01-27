b6553a69e911fdf1f969f6175e5f33d3df0b67ac
Daniyal <daniyal.izadpanahi@gmail.com>
2025-01-27 17:27:25 +0330

    create account app models and admin panel

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'phone_number', 'first_name', 'last_name', 'role', 'is_active', 'created_at')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('role', 'is_active')
+    ordering = ('created_at',)
+    readonly_fields = ("is_deleted","is_email_verified", "is_phone_verified")
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- account/models.py
+++ account/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('shop_admin', 'Shop Admin'),
+        ('user', 'User'),
+    ]
+
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='user')
+    is_active = models.BooleanField(default=True)
+    last_login = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_email_verified = models.BooleanField(default=False)
+    is_phone_verified = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if not self.username:
+            self.username = self.email
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.username
