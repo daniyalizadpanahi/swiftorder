c2ccb61c97ea99fa2eae2b4dc6412009d004021b
Daniyal <daniyal.izadpanahi@gmail.com>
2025-01-29 18:50:22 +0330

    update sending email using celery

---
+++ account/tasks.py
@@ -0,0 +1,8 @@
+from celery import shared_task
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
+
+@shared_task
+def send_email_task(subject, message, recipient_list):
+    send_mail(subject, message, settings.EMAIL_HOST_USER, recipient_list)
--- account/utils.py
+++ account/utils.py
@@ -1,10 +1,8 @@
-from django.core.mail import send_mail
-from django.conf import settings
 from django.utils.http import urlsafe_base64_encode
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
-
+from .tasks import send_email_task
 
 def send_welcome_email(user, request):
     token = default_token_generator.make_token(user)
@@ -14,8 +12,7 @@ def send_welcome_email(user, request):
     subject = "Welcome to our platform!"
     message = render_to_string("welcome_email.html", {"user": user, "link": link})
 
-    send_mail(subject, message, settings.EMAIL_HOST_USER, [user.email])
-
+    send_email_task.delay(subject, message, [user.email])
 
 def send_verification_email(user, request):
     token = default_token_generator.make_token(user)
@@ -25,15 +22,12 @@ def send_verification_email(user, request):
     subject = "Verify your email address"
     message = f"Click the link to verify your email: {link}"
 
-    send_mail(subject, message, settings.EMAIL_HOST_USER, [user.email])
-
+    send_email_task.delay(subject, message, [user.email])
 
 def send_password_reset_email(user, request):
     token = default_token_generator.make_token(user)
     uid = urlsafe_base64_encode(str(user.pk).encode()).decode()
-    reset_link = (
-        f"{get_current_site(request).domain}/account/reset-password/{uid}/{token}/"
-    )
+    reset_link = f"{get_current_site(request).domain}/account/reset-password/{uid}/{token}/"
 
     subject = "بازنشانی رمز عبور"
     message = f"""
@@ -51,4 +45,4 @@ def send_password_reset_email(user, request):
     تیم پشتیبانی
     """
 
-    send_mail(subject, message, settings.EMAIL_HOST_USER, [user.email])
+    send_email_task.delay(subject, message, [user.email])
