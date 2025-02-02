8ce4f993cd4c2178c11006e98dfe50b1efec34e0
Daniyal <daniyal.izadpanahi@gmail.com>
2025-02-02 14:31:36 +0330

    add user manager to user model

--- account/models.py
+++ account/models.py
@@ -1,11 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
+
 
 class CustomUser(AbstractUser):
     ROLE_CHOICES = [
-        ('admin', 'Admin'),
-        ('shop_admin', 'Shop Admin'),
-        ('user', 'User'),
+        ("admin", "Admin"),
+        ("shop_admin", "Shop Admin"),
+        ("user", "User"),
     ]
 
     email = models.EmailField(unique=True)
@@ -13,10 +31,12 @@ class CustomUser(AbstractUser):
     last_name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='user')
+    profile_picture = models.ImageField(
+        upload_to="profile_pics/", blank=True, null=True
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="user")
     is_active = models.BooleanField(default=True)
-    last_login = models.DateTimeField(auto_now=True)
+    last_login = models.DateTimeField(auto_now=True, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     is_email_verified = models.BooleanField(default=False)
     is_phone_verified = models.BooleanField(default=False)
