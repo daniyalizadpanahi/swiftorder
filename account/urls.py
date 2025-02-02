df1d69d73d8b08bdc310f7ca0dca7fa896276129
Daniyal <daniyal.izadpanahi@gmail.com>
2025-02-02 14:33:05 +0330

    additional to last commit

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,28 @@
 from django.urls import path
-from .views import RegisterAPI, UserProfile, SendPasswordResetEmailView, ResetPasswordView
+from .views import (
+    RegisterAPI,
+    UserProfile,
+    SendPasswordResetEmailView,
+    ResetPasswordView,
+    RegisterEmailAPIView,
+    VerifyEmailAPI,
+    DeleteAccountAPI,
+)
 
 app_name = "account"
 
 urlpatterns = [
-    path('register/', RegisterAPI.as_view(), name='register'),
-    path('profile/', UserProfile.as_view(), name='user-profile'),
-    path('password-reset/', SendPasswordResetEmailView.as_view(), name='password_reset_email'),
-    path('reset-password/<str:uidb64>/<str:token>/', ResetPasswordView.as_view(), name='reset_password'),
+    path("register/", RegisterAPI.as_view(), name="register"),
+    path("email-register/", RegisterEmailAPIView.as_view(), name="email_register"),
+    path("verify-email/<str:uidb64>/<str:token>/", VerifyEmailAPI.as_view(), name="verify_email"),
+    path("delete-account/", DeleteAccountAPI.as_view(), name="delete_account"),
+    path("profile/", UserProfile.as_view(), name="user-profile"),
+    path(
+        "password-reset/", SendPasswordResetEmailView.as_view(), name="password_reset"
+    ),
+    path(
+        "password-register/<str:uidb64>/<str:token>/",
+        ResetPasswordView.as_view(),
+        name="password_register",
+    ),
 ]
