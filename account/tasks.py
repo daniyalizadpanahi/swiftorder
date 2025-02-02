4cdf4ec242a94ea149c332b4bd88e0c855795500
Daniyal <daniyal.izadpanahi@gmail.com>
2025-02-02 14:32:11 +0330

    update celery task name

--- account/tasks.py
+++ account/tasks.py
@@ -1,8 +1,7 @@
 from celery import shared_task
 from django.core.mail import send_mail
-from django.template.loader import render_to_string
 from django.conf import settings
 
-@shared_task
+@shared_task(name="send_email_to_user")
 def send_email_task(subject, message, recipient_list):
     send_mail(subject, message, settings.EMAIL_HOST_USER, recipient_list)
