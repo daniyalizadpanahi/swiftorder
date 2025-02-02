b6214b5ca1ed0aaf9c5e20bed7ec257db8eb82bb
Daniyal <daniyal.izadpanahi@gmail.com>
2025-02-02 14:32:40 +0330

    update email task

--- account/utils.py
+++ account/utils.py
@@ -1,33 +1,32 @@
 from django.utils.http import urlsafe_base64_encode
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.sites.shortcuts import get_current_site
-from django.template.loader import render_to_string
 from .tasks import send_email_task
 
+
 def send_welcome_email(user, request):
-    token = default_token_generator.make_token(user)
-    uid = urlsafe_base64_encode(str(user.pk).encode()).decode()
-    link = f"{get_current_site(request).domain}/verify-email/{uid}/{token}/"
-
-    subject = "Welcome to our platform!"
-    message = render_to_string("welcome_email.html", {"user": user, "link": link})
-
+    subject = "به وب‌سایت SwiftOrder خوش آمدید!"
+    message = f"سلام {user.first_name} عزیز،\n\nخوشحالیم که به خانواده SwiftOrder پیوسته‌اید. امیدواریم تجربه خوبی از استفاده از پلتفرم ما داشته باشید.\n\nبا احترام,\nتیم SwiftOrder"
     send_email_task.delay(subject, message, [user.email])
+
 
 def send_verification_email(user, request):
     token = default_token_generator.make_token(user)
-    uid = urlsafe_base64_encode(str(user.pk).encode()).decode()
-    link = f"{get_current_site(request).domain}/verify-email/{uid}/{token}/"
+    uid = urlsafe_base64_encode(str(user.pk).encode())
+    link = f"{get_current_site(request).domain}/account//verify-email/{uid}/{token}/"
 
     subject = "Verify your email address"
     message = f"Click the link to verify your email: {link}"
 
     send_email_task.delay(subject, message, [user.email])
 
+
 def send_password_reset_email(user, request):
     token = default_token_generator.make_token(user)
-    uid = urlsafe_base64_encode(str(user.pk).encode()).decode()
-    reset_link = f"{get_current_site(request).domain}/account/reset-password/{uid}/{token}/"
+    uid = urlsafe_base64_encode(str(user.pk).encode())
+    reset_link = (
+        f"{get_current_site(request).domain}/account/reset-password/{uid}/{token}/"
+    )
 
     subject = "بازنشانی رمز عبور"
     message = f"""
--- account/views.py
+++ account/views.py
@@ -3,19 +3,40 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
-from django.contrib.auth.tokens import PasswordResetTokenGenerator, default_token_generator
+from django.contrib.auth.tokens import (
+    PasswordResetTokenGenerator,
+    default_token_generator,
+)
 from django.contrib.auth import get_user_model
 from django.utils.http import urlsafe_base64_decode
 from django.utils.encoding import force_str
+from django.shortcuts import get_object_or_404
 
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 
 from .serializers import RegisterUserSerializer, UserProfileSerializer
-from .utils import send_verification_email, send_welcome_email, send_password_reset_email
+from .utils import (
+    send_verification_email,
+    send_welcome_email,
+    send_password_reset_email,
+)
 
 
 User = get_user_model()
+
+
+class RegisterEmailAPIView(APIView):
+    def post(self, request):
+        try:
+            user = get_object_or_404(User, pk=request.user.pk)
+            send_verification_email(user, request)
+
+            return Response(
+                {"message": "Verification email sent"}, status=status.HTTP_200_OK
+            )
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class RegisterAPI(mixins.CreateModelMixin, generics.GenericAPIView):
@@ -37,7 +58,9 @@ class RegisterAPI(mixins.CreateModelMixin, generics.GenericAPIView):
         return Response(
             serializer.data, status=status.HTTP_201_CREATED, headers=headers
         )
-
+    def perform_create(self, serializer):
+        user = serializer.save()
+        return user
 
 class UserProfile(
     mixins.RetrieveModelMixin, mixins.UpdateModelMixin, generics.GenericAPIView
@@ -60,21 +83,35 @@ class SendPasswordResetEmailView(APIView):
         request_body=openapi.Schema(
             type=openapi.TYPE_OBJECT,
             properties={
-                'email': openapi.Schema(type=openapi.TYPE_STRING, description='Email address of the user')
-            }
+                "email": openapi.Schema(
+                    type=openapi.TYPE_STRING, description="Email address of the user"
+                )
+            },
         ),
         responses={
-            200: openapi.Response('Password reset email sent!', examples={'application/json': {"message": "Password reset email sent!"}}),
-            404: openapi.Response('User with this email does not exist.', examples={'application/json': {"error": "User with this email does not exist."}})
-        }
+            200: openapi.Response(
+                "Password reset email sent!",
+                examples={
+                    "application/json": {"message": "Password reset email sent!"}
+                },
+            ),
+            404: openapi.Response(
+                "User with this email does not exist.",
+                examples={
+                    "application/json": {
+                        "error": "User with this email does not exist."
+                    }
+                },
+            ),
+        },
     )
     def post(self, request):
         email = request.data.get("email")
         try:
             user = User.objects.get(email=email)
-            
+
             send_password_reset_email(user, request)
-            
+
             return Response(
                 {"message": "Password reset email sent!"}, status=status.HTTP_200_OK
             )
@@ -85,26 +122,47 @@ class SendPasswordResetEmailView(APIView):
             )
 
 
-
 class ResetPasswordView(APIView):
     @swagger_auto_schema(
         operation_description="Resets the user's password using the provided token and user ID.",
-        # مشخص کردن پارامترهای URL
         manual_parameters=[
-            openapi.Parameter('uidb64', openapi.IN_PATH, description="Base64 encoded user ID", type=openapi.TYPE_STRING),
-            openapi.Parameter('token', openapi.IN_PATH, description="Password reset token", type=openapi.TYPE_STRING),
+            openapi.Parameter(
+                "uidb64",
+                openapi.IN_PATH,
+                description="Base64 encoded user ID",
+                type=openapi.TYPE_STRING,
+            ),
+            openapi.Parameter(
+                "token",
+                openapi.IN_PATH,
+                description="Password reset token",
+                type=openapi.TYPE_STRING,
+            ),
         ],
         request_body=openapi.Schema(
             type=openapi.TYPE_OBJECT,
             properties={
-                'new_password': openapi.Schema(type=openapi.TYPE_STRING, description='New password for the user')
-            }
+                "new_password": openapi.Schema(
+                    type=openapi.TYPE_STRING, description="New password for the user"
+                )
+            },
         ),
         responses={
-            200: openapi.Response('Password reset successful!', examples={'application/json': {"message": "Password reset successful!"}}),
-            400: openapi.Response('Invalid or expired token.', examples={'application/json': {"error": "Invalid or expired token."}}),
-            404: openapi.Response('Invalid user.', examples={'application/json': {"error": "Invalid user."}})
-        }
+            200: openapi.Response(
+                "Password reset successful!",
+                examples={
+                    "application/json": {"message": "Password reset successful!"}
+                },
+            ),
+            400: openapi.Response(
+                "Invalid or expired token.",
+                examples={"application/json": {"error": "Invalid or expired token."}},
+            ),
+            404: openapi.Response(
+                "Invalid user.",
+                examples={"application/json": {"error": "Invalid user."}},
+            ),
+        },
     )
     def post(self, request, uidb64, token):
         try:
@@ -152,3 +210,13 @@ class VerifyEmailAPI(APIView):
                 {"detail": "Invalid verification token."},
                 status=status.HTTP_400_BAD_REQUEST,
             )
+
+
+class DeleteAccountAPI(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        user = request.user
+        user.is_deleted = True
+        user.save()
+        return Response({"detail": "Account deleted."}, status=status.HTTP_200_OK)
